1a03bf05043de32635d4125956d9c9fcff104869
Zach0702 <50173310+Zach0702@users.noreply.github.com>
2019-10-07 16:58:41 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LabNumber21.Models;
+
+namespace LabNumber21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return View(register);
+            }
+            return View(register);
+        }
+        //[HttpPost]
+        //public ActionResult Register(Register register)
+        //{
+        //    if (string.IsNullOrEmpty(register.FirstName))
+        //    {
+        //        ModelState.AddModelError("FirstName", "First name is a required field");
+        //    }
+        //    if (string.IsNullOrEmpty(register.LastName))
+        //    {
+        //        ModelState.AddModelError("LastName", "Last name must be entered");
+        //    }
+        //    if (string.IsNullOrEmpty(register.EMail))
+        //    {
+        //        ModelState.AddModelError("EMail", "Email must be entered");
+        //    }
+        //    if (string.IsNullOrEmpty(register.OnlinePassword))
+        //    {
+        //        ModelState.AddModelError("OnlinePassword", "Password must be entered");
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        return View();
+        //    }
+
+        //    else
+        //    {
+        //        return View(register);
+        //    }
+
+        //}
+
+        public IActionResult AddUser(Register userRegister)
+        {
+            return View(userRegister);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabNumber21.Models
+{
+    [Bind, Register]
+    public class Register : Attribute
+    {
+        [Required(ErrorMessage = "Please enter First Name")]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+        
+
+        [Display(Name = "Last Name:")]
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string LastName { get; set; }
+        
+
+        [Display(Name = "E-mail:")]
+        [Required(ErrorMessage = "Please enter a email")]
+        public string EMail { get; set; }
+       
+
+        [Display(Name = "Phone Number:")]
+        [Required(ErrorMessage = "Please enter a phone number")]
+        public int PhoneNumber { get; set; }
+        
+
+        [Display(Name = "Password: ")]
+        [Required(ErrorMessage = "Please enter a password")]
+        public string OnlinePassword { get; set; }
+        
+    }
+}
